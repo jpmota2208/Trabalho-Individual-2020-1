7075627692421a77314c1096f53f9cd8ccf89e22
João Pedro Mota <jpmota.unb@gmail.com>
2020-12-15 10:51:37 -0300

    adding production url condition

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,11 @@
 import axios from 'axios'
 
-const base = 'http://localhost:3000/api/v1'
+const url =
+  process.env.NODE_ENV === 'production'
+    ? 'https://trabackend.herokuapp.com/'
+    : 'https://localhost:3000/'
 
+const base = ${url}/api/v1
 const API = axios.create({
   baseURL: base
 })
