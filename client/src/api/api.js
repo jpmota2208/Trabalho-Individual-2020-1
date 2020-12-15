b5c69d2856543943a50b8d491b4b9a6e25e95aa1
João Pedro Mota <jpmota.unb@gmail.com>
2020-12-15 10:55:58 -0300

    fixing production url

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -3,9 +3,9 @@ import axios from 'axios'
 const url =
   process.env.NODE_ENV === 'production'
     ? 'https://trabackend.herokuapp.com/'
-    : 'https://localhost:3000/'
+    : 'https://localhost:3000'
 
-const base = ${url}/api/v1
+const base = `${url}/api/v1`
 const API = axios.create({
   baseURL: base
 })
